e93a1d43560952a9c4cac481196b7c90835bd5b0
viksingh <vikas.singh.0@gmail.com>
2013-09-10 07:42:18 +1000

    Create Routing.java

---
+++ Routing.java
@@ -0,0 +1,29 @@
+package com.routing.servlet;
+
+
+public class Routing {
+	
+	private String filename;
+	
+	private String bussys;
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getBussys() {
+		return bussys;
+	}
+
+	public void setBussys(String bussys) {
+		this.bussys = bussys;
+	}
+
+	
+	
+
+}
